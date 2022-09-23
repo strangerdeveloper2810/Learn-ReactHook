81ef1beb8590f53d8e9a517d3792cbd94736a8b6
Hải Trình Nguyễn <nguyenmaster3012@gmail.com>
2022-09-23 20:59:47 +0700

    do Feature BetScore

--- src/Exercises/ItemGame.js
+++ src/Exercises/ItemGame.js
@@ -1,18 +1,33 @@
-import React from 'react'
+import React from "react";
+import { useDispatch } from "react-redux";
+import { betScoreActions } from "../redux/actions/BauCuaActions";
 
 export default function ItemGame(props) {
-  const {item} = props;
+  const { item } = props;
+  const dispatch = useDispatch();
   return (
-    <div className='mt-3 ms-3'>
-      <img src={item.img} alt={item.ma} className='w-75' />
+    <div className="mt-3 ms-3">
+      <img src={item.img} alt={item.ma} className="w-75" />
 
       <div className="bg-success mt-2 pb-2 text-center rounded-3 w-75">
-        <button className="btn btn-danger me-3">
+        <button
+          className="btn btn-danger me-3"
+          onClick={() => {
+            dispatch(betScoreActions(item, 1));
+          }}
+        >
           <i className="fa fa-plus"></i>
         </button>
         <span className="mt-2 text-warning fs-3">{item.scoreBet}</span>
-        <button className="btn btn-danger ms-3">-</button>
+        <button
+          className="btn btn-danger ms-3"
+          onClick={() => {
+            dispatch(betScoreActions(item, -1));
+          }}
+        >
+          -
+        </button>
       </div>
     </div>
-  )
+  );
 }
--- src/redux/reducer/BauCuaReducer.js
+++ src/redux/reducer/BauCuaReducer.js
@@ -1,3 +1,4 @@
+import { BET_SCORE } from "../types/BauCuaTypes";
 const initialState = {
   arrBet: [
     { id: "ga", img: "./gameBauCua/ga.png", scoreBet: 0 },
@@ -17,9 +18,26 @@ const initialState = {
 
 const BauCuaReducer = (state = initialState, action) => {
   switch (action.type) {
-    case " ": {
+    case BET_SCORE: {
+      let arrBetUpdate = [...state.arrBet];
+      const index = arrBetUpdate.findIndex(
+        (item) => item.id === action.itemClick.id
+      );
+      if (index !== -1) {
+        if (action.number === 1 && state.totalScore > 0) {
+          arrBetUpdate[index].scoreBet += 100;
+          state.totalScore -= 100;
+        }
+      } else {
+        if (arrBetUpdate[index].scoreBet > 0) {
+          arrBetUpdate[index].scoreBet -= 100;
+          state.totalScore += 100;
+        }
+      }
+      state.arrBet = arrBetUpdate;
       return state;
     }
+
     default:
       return state;
   }
